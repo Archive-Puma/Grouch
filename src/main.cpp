aee291f32fbfc9b7a2204f4183cd2b1639f30648
ROG CosasDePuma <kikefontanlorenzo@gmail.com>
2019-02-28 01:52:39 +0100

    :inbox_tray: Grouch

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "lib\recbinwin10.hpp"
+
+int main
+(int argc, char *argv[])
+{
+	if(argc == 2)
+	{
+		Metadata metadata = unpack((std::string) argv[1]);
+		
+		if(metadata.header == 0x02) {
+			// Show the result
+			std::cout << "Header: " << metadata.header << std::endl;
+			std::cout << "File size: " << metadata.fsize << std::endl;
+			std::cout << "Time stamp: " << metadata.timestamp << std::endl;
+			std::cout << "Name Lenght: " << metadata.namelenght << std::endl;
+			std::cout << "Name: " << metadata.name << std::endl;
+		}
+		
+	} else std::cerr << "Usage: " << argv[0] << " filename" << std::endl;
+	
+	return 0;
+}
+
+// https://stackoverflow.com/questions/26845538/parsing-a-binary-file-what-is-a-modern-way
